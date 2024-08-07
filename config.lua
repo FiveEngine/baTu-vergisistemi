020be11c8827371181e2902ce5e59589dfc1bcc5
baTu <batudevs1316@icloud.com>
2024-08-07 08:41:55 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,4 +2,4 @@ baTu = {}
 
 baTu.VergiKesimAraligi = 60 -- dakika cinsinden 1 saat istiyorsan 60000 yap
 baTu.VebcukuYapistir = 'https://discord.com/api/webhooks/1270614426944540673/so505hYDtR4YvEYI7k2j5tXqFB35TEojA9hvCYJlRP5sI71RkOmWGUZx42y-svu71MFD'
-baTu.SabitVergiTutari = 1000  -- server.lua 32 satırda düzelt
+baTu.SabitVergiTutari = 1000 
